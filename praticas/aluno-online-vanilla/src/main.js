c22675ccb45005377306347f6c409a6d28001b93
Daniel Pacheco Amorim <danielpachecoamorim@gmail.com>
2026-03-24 22:03:22 -0300

    Tela de login - com Erro Desktop

--- praticas/aluno-online-vanilla/src/main.js
+++ praticas/aluno-online-vanilla/src/main.js
@@ -1,1 +1,28 @@
-import './style.css'
+import "./style.css";
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const emailInput = document.querySelector("#email");
+  const emailErro = document.querySelector("#emailErro");
+  const senhaInput = document.querySelector("#senha");
+  const senhaErro = document.querySelector("#senhaErro");
+
+  emailErro.textContent = "";
+  senhaErro.textContent = "";
+
+  if (emailInput.value == "") {
+    emailErro.textContent = "Email é obrigatório";
+    return;
+  }
+
+
+  if (senhaInput.value == "") {
+    senhaErro.textContent = "Senha é obrigatória";
+    return;
+  }
+
+
+  window.location.href="index.html"
+});
