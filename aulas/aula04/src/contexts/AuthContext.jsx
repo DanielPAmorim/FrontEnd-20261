a063223b3ad3a6ea87011ef95a19bbce3a962884
Daniel Pacheco Amorim <danielpachecoamorim@gmail.com>
2026-05-12 21:14:38 -0300

    adiciona contexto de auth

---
+++ aulas/aula04/src/contexts/AuthContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useState } from "react";
+
+// cria o contexto
+const AuthContext = createContext();
+
+// cria o provedor
+function AuthProvider({ children }) {
+  const [logado, setLogado] = useState(false);
+  const [usuario, setUsuario] = useState({});
+
+  const login = (dados) => {
+    //chamar a API passando dados
+    setUsuario({ id: 0, nome: "Daniel", email: "daniel.p.amorim@iesb.edu.br" });
+    setLogado(true);
+  };
+
+  const logout = () => {
+    setUsuario({});
+    setLogado(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ logado, login, logout, usuario }}>
+      {/* value = estado compartihado */}
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export { AuthContext, AuthProvider };
